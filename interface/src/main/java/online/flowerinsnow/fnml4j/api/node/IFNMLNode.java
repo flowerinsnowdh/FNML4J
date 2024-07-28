4bd8f79e89786836fcad3204097b16106a878d3a
flowerinsnow <flowerimsnow@hotmail.com>
2024-07-29 07:46:37 +0800

    修复一处漏写Javadoc的问题 添加LICENSE到jar文件中 转为beta版

--- interface/src/main/java/online/flowerinsnow/fnml4j/api/node/IFNMLNode.java
+++ interface/src/main/java/online/flowerinsnow/fnml4j/api/node/IFNMLNode.java
@@ -13,6 +13,7 @@ public interface IFNMLNode {
      * <p>把当前节点写入流</p>
      * @param offset 缩进
      * @param writer 流
+     * @throws IOException 当调用 {@link Writer#write(String)} 发生 {@link IOException} 时抛出
      */
     void write(int offset, @NotNull Writer writer) throws IOException;
 }
